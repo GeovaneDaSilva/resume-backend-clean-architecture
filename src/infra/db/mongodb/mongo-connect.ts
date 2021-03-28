47d4802e9d8835601b784dcd718feac71e0310e2
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-03-28 01:46:21 -0400

    refactor: change var processenv

--- src/infra/db/mongodb/mongo-connect.ts
+++ src/infra/db/mongodb/mongo-connect.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export default (): void => {
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
+  mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
     if (err) {
       console.log(err.message)
     } else {
