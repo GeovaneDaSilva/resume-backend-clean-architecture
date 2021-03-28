f491fa4132c0b4e7678cc3239c2fd0eeac537e7a
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-03-28 02:23:39 -0400

    refactor: remove to package crache

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,8 @@
 import express from 'express'
-import { config as dotenv } from 'dotenv'
 import middlewares from './middlewares'
 import routes from './routes'
 
 const app = express()
 middlewares(app)
-dotenv()
 routes(app)
 export default app
