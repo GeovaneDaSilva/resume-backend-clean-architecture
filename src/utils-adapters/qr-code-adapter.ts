2a1b4515cde07010cffe3f820a9075bb4e92a038
lucasmonstro <joao.galiano.silva@gmail.com>
2021-03-31 05:54:50 +0100

    refactor(qr-code): remove unused param on QR method

--- src/utils-adapters/qr-code-adapter.ts
+++ src/utils-adapters/qr-code-adapter.ts
@@ -2,7 +2,7 @@ import QRCode from 'qrcode'
 import { IQRCode } from '../presentation/interfaces/qr-code'
 
 export class QRCodeAdapter implements IQRCode {
-  async QR (value: string, object?: any): Promise<any> {
+  async QR (value: string): Promise<any> {
     const code = await QRCode.toDataURL(value)
     return code
   }
