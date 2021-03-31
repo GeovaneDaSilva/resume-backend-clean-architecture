6021b6744f046de493433b34c93788556f1b3763
lucasmonstro <joao.galiano.silva@gmail.com>
2021-03-31 05:58:18 +0100

    refactor(qrcode): remove unused await from adapter

--- src/utils-adapters/qr-code-adapter.ts
+++ src/utils-adapters/qr-code-adapter.ts
@@ -3,7 +3,6 @@ import { IQRCode } from '../presentation/interfaces/qr-code'
 
 export class QRCodeAdapter implements IQRCode {
   async QR (value: string): Promise<string> {
-    const code = await QRCode.toDataURL(value)
-    return code
+    return QRCode.toDataURL(value)
   }
 }
